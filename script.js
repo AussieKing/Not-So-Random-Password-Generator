0a78fd67916926b3768269ec5f8c54910ee4127d
AussieKing <126050763+AussieKing@users.noreply.github.com>
2023-03-30 17:37:54 +1000

    adding the JavaScript file completed

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var passwordText = document.querySelector("#password");
   var passwordLength = parseInt(prompt("How long do you want your password to be? (Between 8 and 128 characters)")); 
   
     // Checking that the the password length is 8 - 128 characters
@@ -16,18 +15,37 @@ function writePassword() {
   var includeNumeric = confirm("Include numeric characters?");
   var includeSpecial = confirm("Include special characters?");
 
+  // here we are defining the arrays of characters to use for each type of character
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
 
+  // let's combine the selected characters into a single string
+  let charsToUse = "";
+  if (includeLowercase) charsToUse += lowercaseChars;
+  if (includeUppercase) charsToUse += uppercaseChars;
+  if (includeNumeric) charsToUse += numericChars;
+  if (includeSpecial) charsToUse += specialChars;
 
+  // Generates the password and uses a (for)loop to select a random character from the combined string
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    // Select a random character (using Math.random) from the combined string
+    let randomIndex = Math.floor(Math.random() * charsToUse.length);
+    let randomChar = charsToUse.charAt(randomIndex);
 
+    // Append the selected character to the password
+    password += randomChar;
+  }
 
-
-
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+//BELOW the acceptance criteria for the password generator:
 //AS AN employee with access to sensitive data
 //I WANT to randomly generate a password that meets certain criteria
 //SO THAT I can create a strong password that provides greater security
